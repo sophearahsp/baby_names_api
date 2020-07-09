d5ae5c7b3f6c35cf1165861ed298cb547e20eebe
sophearahsp <sophearahspsp@gmail.com>
2020-07-09 00:28:15 -0700

    Fixed cors, created actions (index, show, new) for UrlIdentificationsController

--- app/controllers/api/v1/url_identifications_controller.rb
+++ app/controllers/api/v1/url_identifications_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::UrlIdentificationsController < ApplicationController
+    #GET /url_identificiations
+    def index
+        @url_ids = UrlIdentification.all
+        render json: @url_ids
+    end
+
+    #GET /url_identification/#index
+    def show
+        @url_id = UrlIdentification.find(params[:id])
+        render json: @url_id
+    end
+
+    #GET /url_identification/new
+    def new
+        newId = SecureRandom.alphanumeric(12)
+        while UrlIdentification.exists?(identification: newId)
+            newId = SecureRandom.alphanumeric(12)
+        end
+        @identification = UrlIdentification.new(:identification => newId, :created_at => Time.now, :updated_at => Time.now)
+        if @identification.save
+            render json:@identification
+        else
+            render error:{error: 'Unable to insert new identification'}, status: 400
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :url_identifications do
+      resources :url_identifications, only: [:new,:index, :show, :create] do
         resources :name_ideas
       end
     end
