cade0604b3b17d26ac51c802d11e5216a6802066
sophearahsp <sophearahspsp@gmail.com>
2020-07-10 20:52:58 -0700

    Allow index and creation of names

--- app/controllers/api/v1/name_ideas_controller.rb
+++ app/controllers/api/v1/name_ideas_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::NameIdeasController < ApplicationController
+    #GET /url_identificiations/#url_identification_id/name_ideas
+    def index
+        @url_id = UrlIdentification.find(params[:url_identification_id])
+        @name_ideas = @url_id.name_ideas.all
+        render json: @name_ideas
+    end
+
+    #POST /url_identificiations/#url_identification_id/name_ideas
+    def create
+        @newName = NameIdea.new(name_params)
+        if @newName.save
+            render json:@newName
+        else
+            render error:{error: "bad"}, status:400
+        end
+    end
+
+    private
+
+    def name_params
+        params.require(:name_idea).permit(:name, :url_identification_id)
+    end
 end
